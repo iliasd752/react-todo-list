58037502e0c0592d2b7da1458022f180aaf0be88
Ilias Dinia <iliasd752@gmail.com>
2022-11-04 20:00:58 +0000

    First commit

---
+++ src/js/component/ToDoForm/ToDoForm.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+const ToDoForm = () => {
+  return (
+  <form className='todo-form'>
+    <input className='todo-input'/>
+  </form>
+)};
+
+ToDoForm.propTypes = {};
+
+ToDoForm.defaultProps = {};
+
+export default ToDoForm;
